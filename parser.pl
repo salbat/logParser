6bfc060eb263fdf4d2f0e7e0a2a74a9de5a02e90
Sal <0xack13@gmail.com>
2017-11-16 20:48:58 +1100

    uncommented

--- parser.pl
+++ parser.pl
@@ -8,15 +8,8 @@ use Geo::IPfree;
 my ($name, $path, $suffix);
 my $filename = "log.log";
 my $count, %countries;
-
 my $geo = Geo::IPfree->new;
-
 my %data = ();
-
-print "\$data{'John Paul'} = $data{'John Paul'}\n";
-print "\$data{'Lisa'} = $data{'Lisa'}\n";
-print "\$data{'Kumar'} = $data{'Kumar'}\n";
-
 
 ( $name, $path, $suffix ) = fileparse( $filename, "\.[^+.]*" );
 
@@ -30,20 +23,10 @@ open (LOGFILE,"$filename") || die "  Error opening log file $filename.\n";
 
 while(defined($line = <LOGFILE>)) {
     chomp;
-    #print "$line\n";
-
-
     if ($line =~ /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/) {
         $count++;
-        #print "$1\n";
-
         my( $code, $country ) = $geo->LookUp( $1 );
-
-        #print "Country: $country\n" . "Country Code: $code\n";
-
         $data{$country}++;
-        #print "$country occurred: $data{$country}\n";
-
     }
 }
 
