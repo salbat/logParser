43f89b7ad2188ba3441f5434e6678a7230efe5c4
Sal <0xack13@gmail.com>
2017-11-16 20:48:14 +1100

    Code is ready commented

--- parser.pl
+++ parser.pl
@@ -2,10 +2,21 @@
 #use strict;
 #use warnings;
 use File::Basename;
+use Geo::IPfree;
+
 
 my ($name, $path, $suffix);
 my $filename = "log.log";
-my $count;
+my $count, %countries;
+
+my $geo = Geo::IPfree->new;
+
+my %data = ();
+
+print "\$data{'John Paul'} = $data{'John Paul'}\n";
+print "\$data{'Lisa'} = $data{'Lisa'}\n";
+print "\$data{'Kumar'} = $data{'Kumar'}\n";
+
 
 ( $name, $path, $suffix ) = fileparse( $filename, "\.[^+.]*" );
 
@@ -24,8 +35,17 @@ while(defined($line = <LOGFILE>)) {
 
     if ($line =~ /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/) {
         $count++;
-        print "$1\n";
+        #print "$1\n";
+
+        my( $code, $country ) = $geo->LookUp( $1 );
+
+        #print "Country: $country\n" . "Country Code: $code\n";
+
+        $data{$country}++;
+        #print "$country occurred: $data{$country}\n";
+
     }
 }
 
 print "Total number of IP hits: $count\n";
+print "$_ $data{$_}\n" for (keys %data);
